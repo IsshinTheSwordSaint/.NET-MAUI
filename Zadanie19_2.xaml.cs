fae09988670cdc72cdf321a77fa64e137c13b28a
IsshinTheSwordSaint <ut_4003@zsp3zamosc.pl>
2026-02-10 13:56:18 +0100

    Zadania 19.2 i 19.3

---
+++ Zadanie19_2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace Zadania;
+
+public partial class Zadanie19_2 : ContentPage
+{
+    public Zadanie19_2()
+    {
+        InitializeComponent();
+        MyDatePicker.MaximumDate = DateTime.Today.AddDays(30);
+    }
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+
+        if (itemPicker.SelectedIndex >= 0)
+        {
+            string selectedItem = itemPicker.SelectedItem.ToString();
+            var selectedDate = MyDatePicker.Date;
+            var selectedTime = MyTimePicker.Time;
+
+            ResultLabel.Text = $"Wizyta u {selectedItem} dnia {selectedDate:dd.MM.yyyy} o godzinie {selectedTime}";
+        }
+        else
+        {
+            ResultLabel.Text = "Nie uzupełniono formularza, proszę uzupełnić";
+        }
+    }
+}
---
+++ Zadanie19_3.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+
+namespace Zadania;
+
+public partial class Zadanie19_3 : ContentPage
+{
+	public Zadanie19_3()
+	{
+        InitializeComponent();
+        _viewModel = new OrderViewModel();
+        BindingContext = _viewModel;
+    }
+    private OrderViewModel _viewModel;
+
+    public class OrderViewModel : INotifyPropertyChanged
+    {
+        private string _selectedPizza;
+        public string selectedPizza
+        {
+            get => _selectedPizza;
+            set
+            {
+                _selectedPizza = value;
+                OnPropertyChanged(nameof(selectedPizza));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    private void onOptionChanged(object sender, EventArgs e)
+    {
+        int pricePerPizza = 0;
+        int quantity = (int)quantityStepper.Value;
+        if (_viewModel.selectedPizza == "25 cm")
+        {
+            pricePerPizza = 20;
+        }
+        else if (_viewModel.selectedPizza == "32 cm")
+        {
+            pricePerPizza = 28;
+        }
+        else if (_viewModel.selectedPizza == "45 cm")
+        {
+            pricePerPizza = 38;
+        }
+        if (ZSerem.IsChecked)
+        {
+            pricePerPizza += 5;
+        }
+        int totalPrice = pricePerPizza * quantity;
+        resultLabel.Text = $"Cena: {totalPrice} zł";
+    }
+}
